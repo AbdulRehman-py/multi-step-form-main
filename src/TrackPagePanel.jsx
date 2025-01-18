9829466322aedb5ee2ab002f5a88f6d5f42033be
abdul <taylorkings30@gmail.com>
2025-01-18 12:02:37 +0500

    track panel completed

--- src/TrackPagePanel.jsx
+++ src/TrackPagePanel.jsx
@@ -1,12 +1,20 @@
-
-
+import "./Panel.css";
 
 const TrackPagePanel = () => {
-    return (
-        <div>
-        <h1>Track Page Panel</h1>
-        </div>
-    );
+  return (
+    <div className="track-panel">
+      <ol>
+        <li>Step 1</li>
+        <h2>Your Info</h2>
+        <li>Step 2</li>
+        <h2>Select Plan</h2>
+        <li>Step 3</li>
+        <h2>Add-Ons</h2>
+        <li>Step 4</li>
+        <h2>Summary</h2>
+      </ol>
+    </div>
+  );
 };
 
 export default TrackPagePanel;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Personal from './Personal.jsx'
+import TrackPagePanel from './TrackPagePanel.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <TrackPagePanel/>
     <Personal/>
   </StrictMode>,
 )
