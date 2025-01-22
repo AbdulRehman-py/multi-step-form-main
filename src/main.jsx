444dc41c57c33de240cc8861616369ca708d3799
abdul <taylorkings30@gmail.com>
2025-01-22 21:30:36 +0500

    on to the interactivity

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 // import AddOn from './AddOn.jsx'
-// import Personal from './Personal.jsx'
+import Personal from './Personal.jsx'
 import TrackPagePanel from './TrackPagePanel.jsx'
 //import Plan from './Plan.jsx'
-import Summary from './Summary.jsx'
+//import Summary from './Summary.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <TrackPagePanel/>
-    <Summary/>
+    <Personal/>
   </StrictMode>,
 )
