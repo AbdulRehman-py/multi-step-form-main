34bbb41227eb76a5275c45bd0d542ea3872f172a
abdul <taylorkings30@gmail.com>
2025-01-22 08:08:10 +0500

    plan component complete

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Personal from './Personal.jsx'
+// import Personal from './Personal.jsx'
 import TrackPagePanel from './TrackPagePanel.jsx'
+import Plan from './Plan.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <TrackPagePanel/>
-    <Personal/>
+    <Plan/>
   </StrictMode>,
 )
